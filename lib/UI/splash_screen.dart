38679d771e7150bd45ef7bdfc9dc4bf430ea8ccb
Anees Ahmad <aneesahmad1022004@gmail.com>
2025-07-21 19:41:36 +0500

    connit

--- lib/UI/splash_screen.dart
+++ lib/UI/splash_screen.dart
@@ -29,7 +29,7 @@ class _SplashScreenState extends State<SplashScreen> {
                 style: TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
-                  color: Colors.orange,
+                  color: Colors.indigo,
                 ),
               ),
               TextSpan(
